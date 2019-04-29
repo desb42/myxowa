7369b6409b658c87d742f004c5afbc859c4cc3ac
desb42 <desb42@googlemail.com>
2019-04-29 17:48:42 +0100

    more files to make the build work

--- xowa_get_and_make.sh
+++ xowa_get_and_make.sh
@@ -16,42 +16,42 @@
 # *** (Mac OS X) java -Xmx256m -d64 -XstartOnFirstThread -jar xowa_dev.jar
 
 # == ENVIRONMENT VARIABLES ==
-JAVA_JDK_VERSION=1.7
+JAVA_JDK_VERSION=1.8
 # PLAT_NAME must be one of the following: windows_64,linux_64,macosx_64
 PLAT_NAME=windows_64
 # ROOT_DIR should be created beforehand, and should be in "/" format
-ROOT_DIR=c:/xowa_dev
+ROOT_DIR=g:/myxowa_dev
 # these directories MUST be changed to wherever they exist on your system
-ANT_BINARY=/cygdrive/c/dev/apache_ant/bin/ant
-export JAVA_HOME=/cygdrive/c/dev/java/jdk_1_7_x64
+ANT_BINARY=/cygdrive/g/apache-ant-1.10.5/bin/ant
+export JAVA_HOME=/cygdrive/g/Java/jdk1.8.0_181
 
-# == REMOVE PREVIOUS ARTIFACTS ==
-rm -rf ./master
-rm -rf ./src
-rm -rf ./dev
-rm -rf ./bin
-rm -rf ./file
-rm -rf ./user
-rm -rf ./wiki
-rm -rf ./xowa_dev.jar
-
-# == DOWNLOAD XOWA SOURCE ==
-# get src and unzip
-curl -LO https://api.github.com/repos/gnosygnu/xowa/tarball/master
-mkdir src
-# unzip; note that "strip 1" removes container folder EX: gnosygnu-123456
-tar -zxf master -C ./src --strip 1
-
-# == REORGANIZE DOWNLOAD FILES ==
-# move /res/ to /root/ folder
-mv src/res/* .
-# needed for Windows only (cygwin defaults to non-execute permissions)
-chmod -R 755 ./bin/$PLAT_NAME/xulrunner
-
-# remove test files
-find ./src -type f -name '*_tst.java' -delete
-find ./src -type f -name '*_fxt.java' -delete
-find ./src -type f -name '*_mok.java' -delete
+## == REMOVE PREVIOUS ARTIFACTS ==
+#rm -rf ./master
+#rm -rf ./src
+#rm -rf ./dev
+#rm -rf ./bin
+#rm -rf ./file
+#rm -rf ./user
+#rm -rf ./wiki
+#rm -rf ./xowa_dev.jar
+#
+## == DOWNLOAD XOWA SOURCE ==
+## get src and unzip
+#curl -LO https://api.github.com/repos/desb42/myxowa/tarball/master
+#mkdir src
+## unzip; note that "strip 1" removes container folder EX: gnosygnu-123456
+#tar -zxf master -C ./src --strip 1
+#
+## == REORGANIZE DOWNLOAD FILES ==
+## move /res/ to /root/ folder
+#mv src/res/* .
+## needed for Windows only (cygwin defaults to non-execute permissions)
+#chmod -R 755 ./bin/$PLAT_NAME/xulrunner
+#
+## remove test files
+#find ./src -type f -name '*_tst.java' -delete
+#find ./src -type f -name '*_fxt.java' -delete
+#find ./src -type f -name '*_mok.java' -delete
 
 # == BUILD XOWA ==
 $ANT_BINARY -v -buildfile ./dev/make/ant/xowa_compile.xml  -Droot_dir=$ROOT_DIR -Djdk=$JAVA_JDK_VERSION 
