453d2d81f308c00e04a45702c65036f84bf0cd4f
desb42 <desb42@googlemail.com>
2021-10-26 16:57:42 +0100

    remove redundant ' import gplx.xowa.drds.ios.*;'

--- 400_xowa/src/gplx/xowa/drds/ios/media_scanners/Xod_media_scanner__base.java
+++ 400_xowa/src/gplx/xowa/drds/ios/media_scanners/Xod_media_scanner__base.java
@@ -13,7 +13,7 @@ The terms of each license can be found in the source code repository:
 GPLv3 License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-GPLv3.txt
 Apache License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-APACHE2.txt
 */
-package gplx.xowa.drds.ios.media_scanners; import gplx.*; import gplx.xowa.*; import gplx.xowa.drds.*; import gplx.xowa.drds.ios.*;
+package gplx.xowa.drds.ios.media_scanners; import gplx.*; import gplx.xowa.*; import gplx.xowa.drds.*;
 public abstract class Xod_media_scanner__base implements Xod_media_scanner {
 	private final    List_adp list = List_adp_.New();
 	public Xod_media_scanner__base() {
