a3aaa243aa44a6a75c4e4087fa9f92da2f45c538
desb42 <desb42@googlemail.com>
2021-10-26 15:45:46 +0100

    remove redundant ' import gplx.xowa.addons.apps.cfgs.mgrs.*;'

--- 400_xowa/src/gplx/xowa/addons/apps/cfgs/mgrs/execs/Xocfg_exec_mgr.java
+++ 400_xowa/src/gplx/xowa/addons/apps/cfgs/mgrs/execs/Xocfg_exec_mgr.java
@@ -13,7 +13,7 @@ The terms of each license can be found in the source code repository:
 GPLv3 License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-GPLv3.txt
 Apache License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-APACHE2.txt
 */
-package gplx.xowa.addons.apps.cfgs.mgrs.execs; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.apps.cfgs.*; import gplx.xowa.addons.apps.cfgs.mgrs.*;
+package gplx.xowa.addons.apps.cfgs.mgrs.execs; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.apps.cfgs.*;
 public class Xocfg_exec_mgr {
 	private final    Ordered_hash hash = Ordered_hash_.New();
 	public void Add(Gfo_invk invk, String... cmds) {
