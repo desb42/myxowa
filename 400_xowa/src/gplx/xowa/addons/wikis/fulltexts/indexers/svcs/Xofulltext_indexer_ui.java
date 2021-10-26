c5969472120152c31f4272750627120624eb69aa
desb42 <desb42@googlemail.com>
2021-10-26 16:25:51 +0100

    remove redundant ' import gplx.xowa.addons.wikis.fulltexts.indexers.*;'

--- 400_xowa/src/gplx/xowa/addons/wikis/fulltexts/indexers/svcs/Xofulltext_indexer_ui.java
+++ 400_xowa/src/gplx/xowa/addons/wikis/fulltexts/indexers/svcs/Xofulltext_indexer_ui.java
@@ -13,7 +13,7 @@ The terms of each license can be found in the source code repository:
 GPLv3 License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-GPLv3.txt
 Apache License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-APACHE2.txt
 */
-package gplx.xowa.addons.wikis.fulltexts.indexers.svcs; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.wikis.fulltexts.*; import gplx.xowa.addons.wikis.fulltexts.indexers.*;
+package gplx.xowa.addons.wikis.fulltexts.indexers.svcs; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.wikis.fulltexts.*;
 import gplx.xowa.guis.cbks.*;
 public class Xofulltext_indexer_ui {
 	private final    Xog_cbk_mgr cbk_mgr;
