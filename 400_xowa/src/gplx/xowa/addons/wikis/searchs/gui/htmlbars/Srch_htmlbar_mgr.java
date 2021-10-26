867ce07e4c2770026d641ee654106f7dea20eae7
desb42 <desb42@googlemail.com>
2021-10-26 16:35:11 +0100

    remove redundant ' import gplx.xowa.addons.wikis.searchs.gui.*;'

--- 400_xowa/src/gplx/xowa/addons/wikis/searchs/gui/htmlbars/Srch_htmlbar_mgr.java
+++ 400_xowa/src/gplx/xowa/addons/wikis/searchs/gui/htmlbars/Srch_htmlbar_mgr.java
@@ -13,7 +13,7 @@ The terms of each license can be found in the source code repository:
 GPLv3 License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-GPLv3.txt
 Apache License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-APACHE2.txt
 */
-package gplx.xowa.addons.wikis.searchs.gui.htmlbars; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.wikis.searchs.*; import gplx.xowa.addons.wikis.searchs.gui.*;
+package gplx.xowa.addons.wikis.searchs.gui.htmlbars; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.wikis.searchs.*;
 import gplx.xowa.addons.wikis.searchs.searchers.*; import gplx.xowa.addons.wikis.searchs.searchers.cbks.*; import gplx.xowa.addons.wikis.searchs.searchers.rslts.*;
 public class Srch_htmlbar_mgr implements Gfo_invk {
 	private int results_max = 25;
