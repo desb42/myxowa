c077a5494c8721df209c767d36952154905ee4cd
desb42 <desb42@googlemail.com>
2021-10-26 16:32:53 +0100

    remove redundant ' import gplx.xowa.addons.wikis.htmls.*;'

--- 400_xowa/src/gplx/xowa/addons/wikis/htmls/css/dbs/Xowd_css_file_tbl.java
+++ 400_xowa/src/gplx/xowa/addons/wikis/htmls/css/dbs/Xowd_css_file_tbl.java
@@ -13,7 +13,7 @@ The terms of each license can be found in the source code repository:
 GPLv3 License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-GPLv3.txt
 Apache License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-APACHE2.txt
 */
-package gplx.xowa.addons.wikis.htmls.css.dbs; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; import gplx.xowa.addons.wikis.htmls.*; 
+package gplx.xowa.addons.wikis.htmls.css.dbs; import gplx.*; import gplx.xowa.*; import gplx.xowa.addons.*; 
 import gplx.dbs.*;
 public class Xowd_css_file_tbl implements Rls_able {
 	private final    String tbl_name = "css_file"; private final    Dbmeta_fld_list flds = new Dbmeta_fld_list();
